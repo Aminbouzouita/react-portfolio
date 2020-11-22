16ec86f53ac837286064d0bece5f2d2b16b35c9e
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-11-22 00:41:07 -0800

    adding lazy loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { lazy } from "react";
+
 
 function App() {
- 
+    const location = useLocation();
+    const Projects = lazy(() => import("./pages/Projects/Projects"));
+    const Skills = lazy(() => import("./pages/Skills/Skills"));
+    const About = lazy(() => import("./pages/About/About"));
+    const Home = lazy(() => import("./pages/Home/Home"));
+    const NavTabs = lazy(() => import("./components/NavTabs/NavTab/NavTab"));
+    const Footer = lazy(() => import("./components/Footer/Footer"))
+
 }
 
 export default App;
