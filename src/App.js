ffbae01c1bd163891049329015e66fd8dd9df46b
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-11-22 00:48:52 -0800

    adding AnimationPresence

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { lazy } from "react";
 import { BrowserRouter as Router, Link, Switch, useLocation, Route } from "react-router-dom";
-
+import { AnimatePresence } from "framer-motion"
 
 function App() {
     const location = useLocation();
@@ -15,12 +15,14 @@ function App() {
 
         <div>
             <NavTabs />
-            <Switch >
-                <Route exact path="/" component={Home} />
-                <Route exact path="/About" component={About} />
-                <Route exact path="/Projects" component={Projects} />
-                <Route exact path="/Skills" component={Skills} />
-            </Switch>
+            <AnimatePresence exitBeforeEnter>
+                <Switch location={location} key={location.pathname}>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/About" component={About} />
+                    <Route exact path="/Projects" component={Projects} />
+                    <Route exact path="/Skills" component={Skills} />
+                </Switch>
+            </AnimatePresence>
             <Footer />
         </div>
     );
