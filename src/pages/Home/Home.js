e82c5945dc709e43c7c0300bafcb5af670aad7b9
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-11-22 01:05:53 -0800

    adding intersection observer

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import { motion } from "framer-motion"
 import "./style.css";
+import { useRef, useLayoutEffect, useState, useEffect } from 'react'
+
+
 
 export default function Home() {
  
+  const ref = useRef();
+  // const onScreen = useOnScreen(ref, '-100px');
+  // function useOnScreen(ref, rootMargin = '100px') {
+  //   // State and setter for storing whether element is visible
+  //   const [isIntersecting, setIntersecting] = useState(false);
+  
+  //   useEffect(() => {
+  //     const observer = new IntersectionObserver(
+  //       ([entry]) => {
+  //         // Update our state when observer callback fires
+  //         setIntersecting(entry.isIntersecting);
+  //       },
+  //       {
+  //         rootMargin
+  //       }
+  //     );
+  //     if (ref.current) {
+  //       observer.observe(ref.current);
+  //     }
+  //     return () => {
+  //       observer.unobserve(ref.current);
+  //     };
+  //   }, []); // Empty array ensures that effect is only run on mount and unmount
+  
+  //   return isIntersecting;
+
+  // }
     // const pageVariants = {
     //     in: {
     //       opacity: 1,
