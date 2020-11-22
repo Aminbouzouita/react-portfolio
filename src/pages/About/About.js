a4ec807d551d85a47c7f4881c79e97c32983fff0
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-11-22 01:08:56 -0800

    adding about page

---
+++ src/pages/About/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./style.css";
+
+export default function About() {
+  
+  
+  return (
+    <div className="posts">
+      <h1 className="content-subhead">INTRODUCTION</h1>
+      <section className="post">
+        <div className="post-description" id="intro">
+          <p className="introduction">
+            Welcome to my page , I am Amin Bouzouita, I am a Junior Full Stack Web Developer certified from Bootcamp Program at UC Irvine to enhance my skills and learn the lastest Technologies for Javascript, HTML5, CSS3, MySQL, MongoDB, JQuery, React.
+                                As a web developer I do not simply want to create excellent projects, but collaborate with ethical businesses and communities to leave a positive outcome for the current and future generations with a passion of building world className web applications.Currently i am looking for new Web Development opportunities.
+              </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+
