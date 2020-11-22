c3fce1f9456484f141f342e25c711460f7387a37
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-11-22 01:02:01 -0800

    adding home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { motion } from "framer-motion"
+import "./style.css";
+import { useLayoutEffect, useState, useEffect } from 'react'
+
+
+
+export default function Home() {
+
+  return (
+    <main className="posts">
+      <section className="post">
+        <div className="row skills post-description">
+          <div className="project col-6" >
+          </div>
+          <div className="col-6 name">
+            <h1>Amin</h1>
+            <h1>Bouzouita</h1>
+            <p>__</p>
+            <p>____</p>
+            <h1>Full Stuck</h1>
+            <h1>Web Developper</h1>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+
+
+
