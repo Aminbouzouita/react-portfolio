e0a2fd95623ed6fe5ddb51a517c6d8db3c7d6c8e
Amin Bouzouita <Aminbouzouita@yahoo.com>
2021-01-15 17:22:25 -0800

    adding links and logos contact

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,21 +4,41 @@ import githubLogo from "./../../assets/img/25231.png"
 import phoneLogo from "./../../assets/img/844e8cd4ab26c82286238471f0e5a901.png";
 import mailLogo from "./../../assets/img/8fc37b74b608a622588fbaa361485f32.png";
 import linkedlnLogo from "./../../assets/img/ce093c7214ad357bb665cfd2f66a8b6b.png";
+import Link from "../Link/Link";
+import Image from "../Image/Image";
 
 function Footer() {
     return (
-        <div class="row footer">
-            <div className="col-12 footer-form">
-                <a href="https://github.com/Aminbouzouita" target="-blank"> <img alt="" className="img-contact" src={githubLogo} /></a>
-                <a href="tel:001-714-329-3765"> <img alt="" className="img-contact" src={phoneLogo} /></a>
-                <a href="mailto:aminbouzouita@yahoo.com" target="-blank"> <img alt="" className="img-contact" src={mailLogo} /></a>
-                <a href="https://www.linkedin.com/in/amin-bouzouita-53bb911aa/" target="-blank"> <img alt="" className="img-contact" src={linkedlnLogo} /></a>
-            </div>
-            <div className="col-12">
-                <p class="text-footer">Copyright &copy; 2020 Amin Bouzouita</p>
-            </div>
-        </div>
+        <>
 
+                <Link href="https://github.com/Aminbouzouita">
+                    <Image
+                        className="contact-links"
+                        src={githubLogo}
+                        alt="contact-logos"
+                    /></Link>
+                <Link href="tel:001-714-329-3765">
+                    <Image
+                        className="contact-links"
+                        src={phoneLogo}
+                        alt="contact-logos"
+                    />
+                </Link>
+                <Link href="mailto:aminbouzouita@yahoo.com" >
+                    <Image
+                        className="contact-links"
+                        src={mailLogo}
+                        alt="contact-logos"
+                    />
+                </Link>
+                <Link href="https://www.linkedin.com/in/amin-bouzouita-53bb911aa/">
+                    <Image
+                        className="contact-links"
+                        src={linkedlnLogo}
+                        alt="contact-logos"
+                    />
+                </Link>
+          </> 
     );
 }
 
