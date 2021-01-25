84027abf66e6967af030defea1eb35705c59e009
Amin Bouzouita <Aminbouzouita@yahoo.com>
2021-01-24 20:21:27 -0800

    adding slide top component

---
+++ src/components/animations/SlideTop.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { motion } from "framer-motion";
+export default function SlideTop(props) {
+    const pageVariants = {
+        in: {
+            opacity: 1,
+            y: 0,
+            scale: 1
+        },
+        out: {
+            opacity: 0,
+            y: "-100%",
+            scale: 0.8
+        }
+    }
+    const pageTransition = {
+        type: "tween",
+        ease: "anticipate",
+        duration: "2"
+    }
+
+    return (
+
+        <motion.div
+            initial="out"
+            animate="in"
+            exit="out"
+            variants={pageVariants}
+            transition={pageTransition}
+            className={props.className}
+            >
+
+            {props.children}
+
+        </motion.div>
+    )
+}
