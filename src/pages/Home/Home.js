2b101ed389bc757053df01050f17aa8c3b9cb6ac
Amin Bouzouita <Aminbouzouita@yahoo.com>
2021-01-26 17:36:11 -0800

    adding SlideBottom

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import "./style.css";
 import AnimatedText from "./../../components/animations/AnimatedText";
+import SlideBottom from "./../../components/animations/SlideBottom";
+
 export default function Home() {
 
   return (
+    <SlideBottom>
       <AnimatedText
         TextFix="Hi! I'm Amin Bouzouita"
         TopAnimatedText="A Full Stack Web Developer"
         TopAnimatedText="Located in Orange County, CA"
       />
+    </SlideBottom>
   )
 }
