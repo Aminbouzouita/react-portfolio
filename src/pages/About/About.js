fb418f573e317f2274cf0537ad9aa49e03b929f2
Amin Bouzouita <Aminbouzouita@yahoo.com>
2021-01-27 21:21:01 -0800

    adding about content

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,49 +1,40 @@
 import React from 'react';
-import { motion } from "framer-motion"
-import { useHistory , useLocation, useRouteMatch } from 'react-router-dom';
-
 import "./style.css";
-
+import mypicture from "./../../assets/img/mypicture.jpg"
+import myResume from "./../../assets/img/BouzouitaResume.docx"
 export default function About() {
-  console.log(useHistory());
 
-  const pageVariants = {
-    in: {
-      opacity: 1,
-      x: 0,
-      scale: "1"
-    },
-    out: {
-      opacity: 0,
-      x: "-100vw",
-      scale: "0.8"
-    }
-  }
-  const pageTransition = {
-    type: "tween",
-    ease: "anticipate",
-    duration: "1"
-  }
-  
   return (
-    <motion.div
-      initial="out"
-      animate="in"
-      exit="out"
-      variants={pageVariants}
-      transition={pageTransition}
-      className="posts">
-      <h1 className="content-subhead">INTRODUCTION</h1>
-      <section className="post">
-        <div className="post-description" id="intro">
-          <p className="introduction">
-            Welcome to my page , I am Amin Bouzouita, I am a Junior Full Stack Web Developer certified from Bootcamp Program at UC Irvine to enhance my skills and learn the lastest Technologies for Javascript, HTML5, CSS3, MySQL, MongoDB, JQuery, React.
-                                As a web developer I do not simply want to create excellent projects, but collaborate with ethical businesses and communities to leave a positive outcome for the current and future generations with a passion of building world className web applications.Currently i am looking for new Web Development opportunities.
-              </p>
-        </div>
-      </section>
-    </motion.div>
+    <div className="aboutWrapper">
+      <div className="col-lg-7 bgCard" >
+      </div>
+      <div className="col-lg-4">
+        <img
+          alt="mypicture"
+          src={mypicture}
+          id="mypicture" />
+      </div>
+      <p className="col-lg-5 text"> Welcome to my page, 
+      I am Amin Bouzouita a Junior Full Stack Web 
+      Developer certified from Bootcamp Program at 
+      UC Irvine to enhance my skills and learn the 
+      lastest Technologies for Javascript, HTML5, CSS3,
+      MySQL,MongoDB, JQuery, React.
+      As a web developer I do not simply want to 
+      create excellent projects, but collaborate with 
+      ethical businesses and communities to leave a 
+      positive impact for the current and future 
+      generations with a passion of building 
+      approachable and transforming web applications.
+      Currently I am looking for new Web Development 
+      opportunities.
+        <br />
+        <p className="downloadSection"><a href={myResume} id="textLink"> Donwnload a copy of my resume here</a></p>
+      </p>
+    </div>
   );
 }
+
+
 
 
